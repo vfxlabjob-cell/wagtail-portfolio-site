2eb5aee7e031b0f1d5bd466842c21fec8cd23e05
vfxlabjob-cell <vfxlabjob-cell@users.noreply.github.com>
2025-09-18 23:10:52 +0300

    Remove deprecated SiteMiddleware from Wagtail 7.x compatibility

--- mysite/mysite/settings/no_csrf.py
+++ mysite/mysite/settings/no_csrf.py
@@ -26,7 +26,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
