4d1d7b6e81119976dda79b088ce2bfd74cd11315
vfxlabjob-cell <vfxlabjob-cell@users.noreply.github.com>
2025-09-18 22:08:07 +0300

    Fix static files serving in production

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
 # === ЭТОТ БЛОК МЫ ОСТАВЛЯЕМ, ОН ОТВЕЧАЕТ ЗА КАРТИНКИ ===
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # =========================================================
 
 # Этот блок должен быть САМЫМ ПОСЛЕДНИМ
