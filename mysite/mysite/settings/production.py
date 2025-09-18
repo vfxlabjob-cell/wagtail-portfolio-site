a24bdc94980b8f40aa01f45654dfb4d5b9fd490f
vfxlabjob-cell <vfxlabjob-cell@users.noreply.github.com>
2025-09-18 23:07:56 +0300

    Fix wagtail.core imports for Wagtail 7.x compatibility

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
+    "wagtail.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
